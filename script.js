814081243b32a398fe5dc4c11c91470fb68b6693
unknown <rahmadaziz288@gmail.com>
2023-04-09 10:18:50 +0700

    adding message

--- script.js
+++ script.js
@@ -10,3 +10,26 @@ x.onclick = () => {
 y.onclick = () => {
     sideBar.classList.toggle('hide');
 };
+
+
+
+
+
+const scriptURL = 'https://script.google.com/macros/s/AKfycbx6ILYSitaBfUQTikV0rG7gsU8MUJLwdH5BPAEqi-vPwnrhNAmJDQ9adM5LBh_RVyN2/exec'
+const form = document.forms['submit-to-google-sheet']
+const btnSubmit = document.querySelector('.buttonSubmit');
+
+const thanks = document.querySelector('.thanks');
+
+
+form.addEventListener('submit', e => {
+e.preventDefault()
+fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+    .then((response) => {
+        console.log('Success!', response);
+        form.reset();
+
+        thanks.classList.toggle('dNone');
+    })
+    .catch(error => console.error('Error!', error.message))
+});
